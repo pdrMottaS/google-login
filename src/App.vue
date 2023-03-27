bba9a5901f8108177017d28d2411361785fb3ce7
Pedro Motta <pedromotta321r@gmail.com>
2023-03-27 18:51:34 -0300

    add refresh token

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Faz Login ai</h1>
   <button @click="login()">E la vamos nós</button>
+  <button @click="generateAccesstoken()">Será se funciona ?</button>
   <ul v-if="isAuth">
     <li v-for="f in files" :key="f.id">
       {{f.name+" ("+f.id+")"}}
@@ -22,17 +23,36 @@ export default {
     login(){
       {
         googleSdkLoaded((google) => {
-          google.accounts.oauth2.initTokenClient({
+          google.accounts.oauth2.initCodeClient({
             client_id: '532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com',
             scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.metadata https://www.googleapis.com/auth/drive.photos.readonly',
             callback: (response) => {
               console.log(response)
-              window.localStorage.setItem("auth",JSON.stringify(response))
-              this.listFolders()
+              var data = {
+                code:response.code,
+                client_id:"532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com",
+                client_secret:"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",
+                redirect_uri:"http://localhost:5173",
+                grant_type:"authorization_code"
+              }
+              axios.post("https://oauth2.googleapis.com/token",data)
+              .then(res=>{
+                window.localStorage.setItem("auth",JSON.stringify(res.data))
+              })
             }
-          }).requestAccessToken()
+          }).requestCode()
         })
       }
+    },
+    async generateAccesstoken(){
+      var data = {
+        refresh_token:JSON.parse(window.localStorage.getItem("auth")).refresh_token,
+        client_id:"532089225272-1im33klerc0hmvspgo6mh08aobithavt.apps.googleusercontent.com",
+        client_secret:"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",
+        grant_type:"refresh_token"
+      }
+      const res = await axios.post("https://oauth2.googleapis.com/token",data)
+      console.log(res.data)
     },
     async listFolders(){
       const auth = JSON.parse(window.localStorage.getItem("auth"))
